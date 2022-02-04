0982073c770f1f2e93841aa95b3655a184ac6a6d
Nicolas Mortelliti <npmortelliti@gmail.com>
2022-02-03 17:15:05 -0800

    commit tree f30db0df67cc0607d2c8a51eab43024362bd1a2b

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+// Function declaration that sets a default parameter
+// value that can be overridden.
+function saturdayFun(activity = "roller-skate"){
+  // Return a string invoking function parameter
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// Function expression that sets a default parameter
+// value that can be overridden.
+const mondayWork = function(activity = "go to the office"){
+  // Return a string invoking function parameter
+  return `This Monday, I will ${activity}.`;
+}
+
+// Function with child functions. Example of 
+// scope chain. A total call should be:
+// wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!"
+function wrapAdjective(flair = "*"){
+  // Return a function that:
+  // - takes single parameter (default --> "special")
+  // - returns string "You are ..."
+  return function (adj = "special"){
+    return `You are ${flair}${adj}${flair}!`;
+  }
+}
